cfd406fa614a40da822639b52838c3b385780e45
MengXinZXZ <2223529500@qq.com>
2025-04-06 18:08:03 +0800

    修改

--- game/update.js
+++ game/update.js
@@ -3,7 +3,7 @@ window.noname_update = {
 	update: "NULL", //新版本更新文件较多，直接强制进行全量更新
 	changeLog: [
 		"解耦前的过渡版本①",
-		"新武将，新函数，bug修复，AI优化与台词调整，具体请查看game/updateLog.md",
+		"新武将，新函数，bug修复，AI优化与台词调整",
 	],
 	files: [],
 };
