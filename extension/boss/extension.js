f30c4f785a529ca53ab970a44c9d74ff6688df61
冉龙玉 <yutongg601@gmail.com>
2025-04-16 16:32:50 +0800

    harmless adjustment

--- extension/boss/extension.js
+++ extension/boss/extension.js
@@ -1,8 +1,8 @@
 import { lib, game, ui, get, ai, _status } from "../../noname.js";
-game.import("play", function () {
+game.import("play", function() {
 	return {
 		name: "boss",
-		init: function () {
+		init() {
 			if (get.mode() == "tafang") return;
 			var storage = localStorage.getItem("boss_storage_playpackconfig");
 			try {
@@ -15,11 +15,7 @@ game.import("play", function () {
 				for (var i in lib.characterPack.boss) {
 					lib.characterPack.boss[i].img = `image/mode/boss/character/${i}.jpg`;
 					lib.character[i] = lib.characterPack.boss[i];
-					if (
-						typeof lib.character[i][2] != "number" &&
-						(typeof lib.character[i][2] != "string" ||
-							lib.character[i][2].indexOf("/") == -1)
-					) {
+					if (typeof lib.character[i][2] != "number" && (typeof lib.character[i][2] != "string" || lib.character[i][2].indexOf("/") == -1)) {
 						lib.character[i][2] = Infinity;
 					}
 					if (!lib.config.boss_enableai_playpackconfig) {
@@ -28,10 +24,7 @@ game.import("play", function () {
 				}
 			}
 			var list2 = storage.versus || {};
-			if (
-				get.mode() != "versus" ||
-				get.config("versus_mode") != "jiange"
-			) {
+			if (get.mode() != "versus" || get.config("versus_mode") != "jiange") {
 				lib.characterPack.jiange = list2;
 				for (var i in lib.characterPack.jiange) {
 					lib.characterPack.jiange[i].img = `image/mode/versus/character/${i}.jpg`;
@@ -44,20 +37,13 @@ game.import("play", function () {
 					}
 				}
 				lib.characterIntro.boss_liedixuande = lib.characterIntro.liubei;
-				lib.characterIntro.boss_gongshenyueying =
-					lib.characterIntro.huangyueying;
-				lib.characterIntro.boss_tianhoukongming =
-					lib.characterIntro.shen_zhugeliang;
-				lib.characterIntro.boss_yuhuoshiyuan =
-					lib.characterIntro.pangtong;
-				lib.characterIntro.boss_qiaokuijunyi =
-					lib.characterIntro.zhanghe;
-				lib.characterIntro.boss_jiarenzidan =
-					lib.characterIntro.caozhen;
-				lib.characterIntro.boss_duanyuzhongda =
-					lib.characterIntro.simayi;
-				lib.characterIntro.boss_juechenmiaocai =
-					lib.characterIntro.xiahouyuan;
+				lib.characterIntro.boss_gongshenyueying = lib.characterIntro.huangyueying;
+				lib.characterIntro.boss_tianhoukongming = lib.characterIntro.shen_zhugeliang;
+				lib.characterIntro.boss_yuhuoshiyuan = lib.characterIntro.pangtong;
+				lib.characterIntro.boss_qiaokuijunyi = lib.characterIntro.zhanghe;
+				lib.characterIntro.boss_jiarenzidan = lib.characterIntro.caozhen;
+				lib.characterIntro.boss_duanyuzhongda = lib.characterIntro.simayi;
+				lib.characterIntro.boss_juechenmiaocai = lib.characterIntro.xiahouyuan;
 			} else if (_status.mode != "jiange") {
 				for (var i in list2) {
 					lib.character[i] = list2[i];
@@ -71,10 +57,10 @@ game.import("play", function () {
 			list.jiange_character_config = "剑阁武将";
 
 			for (var i in list) {
-				lib.translate[i] = lib.translate[i] || list[i];
+				lib.translate[i] ||= list[i];
 			}
 		},
-		arenaReady: function () {
+		arenaReady() {
 			if (get.mode() == "tafang") return;
 			var storage = localStorage.getItem("boss_storage_playpackconfig");
 			try {
@@ -85,12 +71,11 @@ game.import("play", function () {
 			if (!storage.translate) {
 				storage.translate = {};
 			}
-			var loadversus = function () {
+			var loadversus = function() {
 				if (get.mode() != "versus") {
-					game.loadModeAsync("versus", function (mode) {
+					game.loadModeAsync("versus", function(mode) {
 						for (var i in mode.translate) {
-							lib.translate[i] =
-								lib.translate[i] || mode.translate[i];
+							lib.translate[i] ||= mode.translate[i];
 							storage.translate[i] = mode.translate[i];
 						}
 						for (var i in mode.skill) {
@@ -110,23 +95,16 @@ game.import("play", function () {
 								storage.versus[i] = mode.jiangeboss[i];
 							}
 						}
-						localStorage.setItem(
-							"boss_storage_playpackconfig",
-							JSON.stringify(storage)
-						);
+						localStorage.setItem("boss_storage_playpackconfig", JSON.stringify(storage));
 					});
 				} else {
-					localStorage.setItem(
-						"boss_storage_playpackconfig",
-						JSON.stringify(storage)
-					);
+					localStorage.setItem("boss_storage_playpackconfig", JSON.stringify(storage));
 				}
 			};
 			if (get.mode() != "boss") {
-				game.loadModeAsync("boss", function (mode) {
+				game.loadModeAsync("boss", function(mode) {
 					for (var i in mode.translate) {
-						lib.translate[i] =
-							lib.translate[i] || mode.translate[i];
+						lib.translate[i] ||= mode.translate[i];
 						storage.translate[i] = mode.translate[i];
 					}
 					for (var i in mode.skill) {
@@ -140,9 +118,7 @@ game.import("play", function () {
 					}
 					storage.boss = {};
 					for (var i in mode.characterPack.mode_boss) {
-						if (
-							mode.characterPack.mode_boss[i].isBossAllowed
-						) {
+						if (mode.characterPack.mode_boss[i].isBossAllowed) {
 							storage.boss[i] = mode.characterPack.mode_boss[i];
 						}
 					}
