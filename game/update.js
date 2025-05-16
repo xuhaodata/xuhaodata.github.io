cb57d51da2210f64bd01f5376a7c16332c2f05c2
Rintim <rintim@foxmail.com>
2025-05-16 21:00:21 +0800

    chore: v1.10.17.2 update.

--- game/update.js
+++ game/update.js
@@ -1,9 +1,11 @@
 window.noname_update = {
-	version: "1.10.17.1",
+	version: "1.10.17.2",
 	update: "NULL", //新版本更新文件较多，直接强制进行全量更新
 	changeLog: [
-		"解耦前的过渡版本①",
-		"新武将，新函数，bug修复，AI优化与台词调整",
+		"国战更新前的过渡版本",
+		"新武将，新函数，bug修复，AI优化与台词调整，具体请查看game/updateLog.md",
+		"添加“自动导入扩展”选项，具体内容参考game/updateLog.md下的说明",
+		"为未来的武将解耦做准备"
 	],
 	files: [],
 };
